23dc19e5ae0de57ee319a7897665ca54e8e47386
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-07-16 10:50:46 -0500

    added 2 toys in toy array and logged toy names to the console

--- leonids-toys.js
+++ leonids-toys.js
@@ -1,49 +1,46 @@
-/* const steveBrownleeDoll = {
-    id: 1,
-    name: "Steve Brownlee Doll",
-    price: 12.99,
-    isElectric: true,
-    amountInStock : 7,
-}
-
-const blueToyCar = {
-    id: 2,
-    name: "Blue Toy Car",
-    price: 3.99,
-    isElectric: false,
-    amountInStock: 3,
-}
-
-const beePlushie = {
-    id: 3,
-    name: "Bee Plushie",
-    price: 16.99
-    isElectric: false,
-    amountInStock: 8,
-}*/
 const toys = [
 {
     id: 1,
     name: "Steve Brownlee Doll",
     price: 12.99,
-    isElectric: true,
+    needsBatteries: true,
     amountInStock : 7,
 },
 {
     id: 2,
     name: "Blue Toy Car",
     price: 3.99,
-    isElectric: false,
+    needsBatteries: false,
     amountInStock: 3,
 },
 {
     id: 3,
     name: "Bee Plushie",
     price: 16.99,
-    isElectric: false,
+    needsBatteries: false,
     amountInStock: 8,
-}
+},
 ];
+
+const hotWheelsTrack = {
+    id: 4,
+    name: "Hot Wheels Racing Track",
+    price: 29.99,
+    needsBatteries: true,
+    amountInStock: 1,
+}
+
+const hotWheels20CarPack = {
+    id: 5,
+    name: "Hot Wheels 20-Car Pack",
+    price: 23.99,
+    needsBatteries: false,
+    amountInStock: 4,
+}
+
+toys.push(hotWheelsTrack);
+toys.push(hotWheels20CarPack);
+
 
 for (const toy of toys) {
     console.log(toy.name);
